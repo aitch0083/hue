31491b53ac88685dcfcab34eefd56ea38105c646
Aitch Chung <aitch@dewtouch.com>
2017-04-26 14:22:45 +0800

    Added HTML sanitization

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -196,7 +196,7 @@ module.exports = function(validator){
 					allowedTags: [ 
 					  'h1','h2','h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
 					  'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'hr', 'br', 'div', 'img',
-					  'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre' 
+					  'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe'
 					],
 					allowedAttributes: {
 					  a: [ 'href', 'name', 'target', 'title'],
