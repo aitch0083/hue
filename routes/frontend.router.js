b8ae8afae98c812ca9f944207ce9b5101ccb242b
Aitch Chung <aitch@dewtouch.com>
2017-10-31 16:08:10 +0800

    Added updateTimeUnix

--- routes/frontend.router.js
+++ routes/frontend.router.js
@@ -527,10 +527,11 @@ router.get('/articles/lineRssFeed/:cate_id/:page_size', function(req, res, next)
 
 			_.each(articles, function(article){
 
-				var URI        = configs.site_public_url + '/articles/read/' + article.id + '.html';
-				var ID         = 'WWWLIANCARCOMARTICLE'+article.id;
-				var start_time = article.start_time && article.start_time !== '0000-00-00 00:00:00' ? moment(article.start_time, 'YYYY-MM-DD HH:mm:ss') : moment(article.created, 'YYYY-MM-DD HH:mm:ss');
-				var end_time   = start_time.add('1', 'year');
+				var URI         = configs.site_public_url + '/articles/read/' + article.id + '.html';
+				var ID          = 'WWWLIANCARCOMARTICLE'+article.id;
+				var start_time  = article.start_time && article.start_time !== '0000-00-00 00:00:00' ? moment(article.start_time, 'YYYY-MM-DD HH:mm:ss') : moment(article.created, 'YYYY-MM-DD HH:mm:ss');
+				var end_time    = start_time.add('1', 'year');
+				var update_time = moment(article.modified, 'YYYY-MM-DD HH:mm:ss');
 
 				if(!article.video_url){
 					var format_content = '<div width="width:100%; max-width: 100%;"><img src="'+article.thumbnail+'" style="width:100%; max-width: 100%;"/></div><div>' + article.abstract + '</div>';
@@ -571,6 +572,7 @@ router.get('/articles/lineRssFeed/:cate_id/:page_size', function(req, res, next)
 				   .ele('title', null, article.plain_title).up()
 				   .ele('category', null, article.Category.get('title')).up()
 				   .ele('publishTimeUnix', null, start_time.valueOf()).up()
+				   .ele('updateTimeUnix', null, update_time.valueOf()).up()
 				   .ele('contentType', null, 0).up()
 				   .ele('thumbnail', null, article.thumbnail).up()
 				   .ele('contents')
