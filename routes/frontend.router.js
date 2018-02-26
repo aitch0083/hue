7888279ae7576f9dcf3843307ff4be18995dec57
Aitch Chung <aitch@dewtouch.com>
2018-02-26 16:23:37 +0800

    Added diplay message

--- routes/frontend.router.js
+++ routes/frontend.router.js
@@ -211,8 +211,8 @@ var category_read_handler = function(req, res, next){
 	).spread(function(category, cate_articles, latest_articles, banners){
 
 		if(!category || category === null || category.display != 1){
-			var error = new Error('Invalid Category');
-			console.error('Invalid category caught:', id);
+			var error = new Error('Invalid Category. ' + category.display);
+			console.error('Invalid category caught:', id, ', display: ', category.display);
 			next(error);
 			return;
 		}
