53476d0682b93007fe930968f193fe151b907061
Aitch Chung <aitch@dewtouch.com>
2018-03-07 09:53:04 +0800

    Remove the Wednesday channel

--- libs/js/fe_footer_script.js
+++ libs/js/fe_footer_script.js
@@ -49,7 +49,7 @@ var loadnext = _.throttle(function(){
 
 					var live_days = {
 						// 1:'週一愛生活', 
-						3:'週三愛玩車'
+						// 3:'週三愛玩車'
 					};
 					var today     = new Date();
 				    var dow       = today.getDay();
